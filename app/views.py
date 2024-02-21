a94a247f02afce87460f3aea725654aa757228aa
DJanae <djanaepatterson07@gmail.com>
2024-02-21 15:12:15 -0500

    'completed'

--- app/views.py
+++ app/views.py
@@ -1,33 +1,47 @@
 from app import app
-from flask import render_template, request, redirect, url_for, flash
+from flask import render_template
+from datetime import datetime
 
+# Sample data for the user profile
+user_profile = {
+    'photo': 'https://via.placeholder.com/150',
+    'full_name': 'D\'Janae Patterson',
+    'username': 'djanaepat',
+    'location': 'Kingston, Jamaica',
+    'join_date': datetime(2024, 2, 19),  # Using datetime object for join date
+    'bio': 'I am smart and intelligent and I love web design.',
+    'num_posts': 10,
+    'num_followers': 100,
+    'num_following': 50,
+}
 
-###
-# Routing for your application.
-###
+def format_date_joined(date):
+    """Format the date as Month, Year (e.g. Feb, 2021)."""
+    return date.strftime('%b, %Y')
 
+# New route and view function for the profile page
+@app.route('/profile/')
+def profile():
+    """Render the user's profile page."""
+    user_profile['join_date'] = format_date_joined(user_profile['join_date'])
+    return render_template('profile.html', user=user_profile)
+
+# Existing routes and view functions
 @app.route('/')
 def home():
     """Render website's home page."""
     return render_template('home.html')
-
 
 @app.route('/about/')
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
-
-###
-# The functions below should be applicable to all Flask apps.
-###
-
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
     """Send your static text file."""
     file_dot_text = file_name + '.txt'
     return app.send_static_file(file_dot_text)
-
 
 @app.after_request
 def add_header(response):
@@ -39,7 +53,6 @@ def add_header(response):
     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
     response.headers['Cache-Control'] = 'public, max-age=0'
     return response
-
 
 @app.errorhandler(404)
 def page_not_found(error):
